eafea17cd3af81e756398d3e15571f38e5b5e307
Chino <agenciakoimdq@gmail.com>
2026-07-30 01:32:39 -0300

    fix: disable autoPan on map popups

--- components/MapaInteractivo.tsx
+++ components/MapaInteractivo.tsx
@@ -169,7 +169,7 @@ export default function MapaInteractivo({
               click: () => onSeleccionar(m.facilitador.id),
             }}
           >
-            <Popup closeButton={true} autoPan={true}>
+            <Popup closeButton={true} autoPan={false}>
               <div className="p-2.5 min-w-[200px] max-w-[260px]">
                 <h3 className="font-serif font-medium text-bark text-sm mb-2 leading-tight">
                   {m.facilitador.nombre}
