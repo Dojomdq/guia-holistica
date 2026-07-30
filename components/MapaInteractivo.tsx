4184075a7c8e5f5afea6a4d054031a7722d66189
Chino <agenciakoimdq@gmail.com>
2026-07-30 01:04:40 -0300

    fix: restore close button on map popup

--- components/MapaInteractivo.tsx
+++ components/MapaInteractivo.tsx
@@ -183,7 +183,7 @@ export default function MapaInteractivo({
               click: () => onSeleccionar(m.facilitador.id),
             }}
           >
-            <Popup closeButton={false} autoPan={true}>
+            <Popup closeButton={true} autoPan={true}>
               <div className="p-2.5 min-w-[200px] max-w-[260px]">
                 <h3 className="font-serif font-medium text-bark text-sm mb-2 leading-tight">
                   {m.facilitador.nombre}
