687287bd1a16ba8d452a3f1615141ce5b4bdbe8d
Chino <agenciakoimdq@gmail.com>
2026-08-10 06:33:47 -0300

    Add PopupReel for Instagram content (preview test)

--- components/PopupManager.tsx
+++ components/PopupManager.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import PopupFacilitadores from "@/components/PopupFacilitadores";
+import PopupReel from "@/components/PopupReel";
 
 export default function PopupManager() {
   return (
     <>
       <PopupFacilitadores onClose={() => {}} />
+      <PopupReel
+        reelUrl="https://www.instagram.com/reel/REEMPLAZAR_CON_TU_REEL/embed/"
+        titulo="🎥 Conocé nuestro trabajo"
+      />
     </>
   );
 }
