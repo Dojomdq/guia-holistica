06200915711c84b35f37b0d2700f56c02773d3b7
Chino <agenciakoimdq@gmail.com>
2026-08-11 11:46:20 -0300

    Remove Instagram reel popup

--- components/PopupManager.tsx
+++ components/PopupManager.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import PopupFacilitadores from "@/components/PopupFacilitadores";
-import PopupReel from "@/components/PopupReel";
 
 export default function PopupManager() {
   return (
     <>
       <PopupFacilitadores onClose={() => {}} />
-      <PopupReel
-        reelUrl="https://www.instagram.com/reel/DaxfX2MK3Ih/embed/"
-        titulo="🎥 Conocé nuestro trabajo"
-      />
     </>
   );
 }
